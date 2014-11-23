8af4b9cee3e77944a187fdd823a97835e4d5ab55
Ole Begemann <ole@oleb.net>
2014-11-23 13:25:41 +0100

    Create new OS X framework project in Xcode

---
+++ SwiftCollections/SwiftCollections.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCollections.h
+//  SwiftCollections
+//
+//  Created by Ole Begemann on 23/11/14.
+//  Copyright (c) 2014 Ole Begemann. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCollections.
+FOUNDATION_EXPORT double SwiftCollectionsVersionNumber;
+
+//! Project version string for SwiftCollections.
+FOUNDATION_EXPORT const unsigned char SwiftCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCollections/PublicHeader.h>
+
+
