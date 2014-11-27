cd169a39679e55f5276ed25ca6710b21c5df4908
Ole Begemann <ole@oleb.net>
2014-11-27 18:20:00 +0100

    Rewrite Set tests with Quick and Nimble

--- SwiftCollectionsTests/SetTests.swift
+++ SwiftCollectionsTests/SetTests.swift
@@ -1,63 +1,50 @@
 import SwiftCollections
-import XCTest
+import Quick
+import Nimble
 
-class SetTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-    }
+class SetSpec: QuickSpec {
     
-    override func tearDown() {
-        super.tearDown()
-    }
-    
-    func testCountForEmptySet() {
-        let emptySet = Set<Int>()
-        XCTAssertEqual(emptySet.count, 0)
-    }
-
-    func testCountForNonEmptySet() {
-        let set = Set([1,2,3,4,5])
-        XCTAssertEqual(set.count, 5)
-    }
-    
-    func testThatDuplicatesAreNotInserted() {
-        // Given
-        var set = Set<Int>()
-        // When
-        set.insert(10)
-        set.insert(10)
-        set.insert(10)
-        // Then
-        XCTAssertEqual(set.count, 1)
-    }
-    
-    func testThatItemsCanBeRemoved() {
-        // Given
-        var set = Set(Array(1...10))
-        // When
-        set.remove(3)
-        set.remove(8)
-        // Then
-        XCTAssertEqual(set.count, 8)
-        XCTAssertFalse(set.contains(3))
-        XCTAssertFalse(set.contains(8))
-        XCTAssertTrue(set.contains(7))
-    }
-    
-    func testSequenceEnumeration() {
-        // Given
-        let items = [11,22,33,44,55,66,77,88,99]
-        let set = Set(items)
-        // When
-        let sortedSetElements = sorted(map(set) { $0 })
-        // Then
-        XCTAssertEqual(sortedSetElements, items)
-    }
-    
-    func testContains() {
-        let set = Set(["Jane", "John", "Kathy", "Michael", "Kelly", "Thomas"])
-        XCTAssertTrue(set.contains("Kathy"))
-        XCTAssertFalse(set.contains("Linda"))
+    override func spec() {
+        
+        describe("a set") {
+            it("counts its elements") {
+                let emptySet = Set<Int>()
+                let set = Set([1,2,3,4,5])
+                expect(emptySet.count) == 0
+                expect(set.count) == 5
+            }
+            
+            it("does not insert duplicate elements") {
+                var set = Set<Int>()
+                set.insert(10)
+                set.insert(10)
+                set.insert(10)
+                expect(set.count) == 1
+            }
+            
+            it("can delete elements") {
+                var set = Set([1,2,3,3,7,8,9,8,8])
+                set.remove(3)
+                set.remove(8)
+                expect(set.count) == 4
+                expect(set.contains(3)).to(beFalse())
+                expect(set.contains(8)).to(beFalse())
+                expect(set.contains(7)).to(beTrue())
+            }
+            
+            it("supports sequence enumeration") {
+                let items = [11,22,33,44,55,66,77,88,99]
+                let set = Set(items)
+                let sortedSetElements = sorted(map(set) { $0 })
+                expect(sortedSetElements).to(equal(items))
+            }
+            
+            it("can look up its elements") {
+                let set = Set(["Jane", "John", "Kathy", "Michael", "Kelly", "Thomas"])
+                expect(set.contains("Kathy"))
+                expect(set.contains("Linda"))
+                expect(set.contains("Ben")).to(beFalse())
+            }
+        }
     }
 }
